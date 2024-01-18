45163786386fa1795907426e4deafaa9304fd43e
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-01-18 17:22:36 +0530

    add price and packages

--- display/models.py
+++ display/models.py
@@ -9,6 +9,21 @@ class Services(models.Model):
     name = models.CharField(max_length=100)
     image = models.FileField(upload_to='services/',max_length=1000,null=True,default=None)
     description = models.TextField()
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
+
+class Packages(models.Model):
+    class Meta:
+        db_table = 'Packages'
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)      
+
 
     def __str__(self):
         return self.name
